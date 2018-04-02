1f21db73e8496a643b7ee298ac37bafc198ae30f
Taras KUHAR <tkuhar@e2r8p7.unit.ua>
2018-04-02 22:10:33 +0300

    123

---
+++ valid.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkuhar <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/01 18:21:44 by tkuhar            #+#    #+#             */
+/*   Updated: 2018/04/01 18:21:45 by tkuhar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct	s_elem
+{
+	struct s_elem	*next;
+	struct s_elem	*prev;
+	int		pos[8];
+}				t_elem;
+
+#endif
