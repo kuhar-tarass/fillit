31df072bff5b19f112994326cacc71a7e9e357d7
Taras KUHAR <tkuhar@e1r10p9.unit.ua>
2018-04-04 21:54:37 +0300

    govnoCode

--- valid.c
+++ valid.c
@@ -6,27 +6,58 @@
 /*   By: tkuhar <tkuhar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/02 17:28:29 by tkuhar            #+#    #+#             */
-/*   Updated: 2018/04/04 18:35:31 by tkuhar           ###   ########.fr       */
+/*   Updated: 2018/04/04 21:54:04 by tkuhar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "valid.h"
 
 
-//##############################################
+//############################################################
+// УЖс, куча говнокода, переписал очень почти всё :)
+//
+//	не отличаю в конце файла 
+//	 ...#$
+//	 ...#$
+//	 ...#$
+//	 ...#$
+//	 от 
+//	 ...#$
+//	 ...#$
+//	 ...#$
+//	 ...#$
+//	 $
+// хз как починить не ломая всё.
+//###########################################################
 #include <stdio.h>
-//##############################################
+//###########################################################
 
+void	ft_elemdel(t_elem **alst)
+{
+	t_elem	*temp;
+
+	if (alst && *alst)
+	{
+		while (*alst)
+		{
+			temp = (*alst)->next;
+			free((*alst)->pos);
+			free(*alst);
+			*alst = temp;
+		}
+		*alst = 0;
+	}
+}
 
 t_elem	*ft_elemnew(int *content)
 {
-	t_elem *new;
-	int i;
+	t_elem	*new;
+	int		i;
 
 	if (!(new = malloc(sizeof(t_elem))))
 		return (0);
 	i = -1;
-	while (++i < 8) 
+	while (++i < 8)
 		new->pos = content;
 	new->next = 0;
 	return (new);
@@ -59,13 +90,13 @@ int	*key(int *map)
 	int *k;
 	int j;
 
-	if (!map || !(k = malloc(8*4)))
+	if (!map || !(k = malloc(8 * 4)))
 		return (0);
 	j = 16;
 	minr = 3;
 	minc = 3;
 	i = 0;
-	while(j--)
+	while (j--)
 		if (map[15 - j])
 		{
 			k[i] = (15 - j) % 4;
@@ -79,7 +110,7 @@ int	*key(int *map)
 	return (k);
 }
 
-int *checkmap(int *map)
+int	*checkmap(int *map)
 {
 	int i;
 	int b;
@@ -99,29 +130,28 @@ int *checkmap(int *map)
 		if (map[i] == 1)
 			return (0);
 	}
-	if (b != 4 || max < 3)
+	if (b != 4 || max < 4)
 		return (0);
 	return (map);
 }
 
-
 int	checkvalid_input(int *map, t_elem **elem, char *s)
 {
-	char buf[20];
-	int j;
-	int *k;
-	int fd;
-	int i;
+	char	buf[20];
+	int		j;
+	int		*k;
+	int		fd;
+	int		i;
 
 	fd = open(s, O_RDONLY);
-	while ( (i = read (fd, buf, 20)) == 20)
+	while ( (i = read(fd, buf, 20)) == 20)
 	{
 		j = 20;
-		while(buf[4] == '\n' && buf[9] == '\n' && buf[14] == '\n' &&
+		while (buf[4] == '\n' && buf[9] == '\n' && buf[14] == '\n' &&
 			buf[19] == '\n' && j--)
 			if (buf[19 - j] == '.' || buf[19 - j] == '#')
 				*map++ = (buf[19 - j] == '.') ? 0 : 1;
-			else if(buf[19 - j] != '\n')
+			else if (buf[19 - j] != '\n')
 				return (1);
 		if (ft_elempush_back(elem, key(checkmap(map = map - 16))))
 				return (1);
@@ -135,20 +165,26 @@ int	checkvalid_input(int *map, t_elem **elem, char *s)
 
 int	main(int argc, char **argv)
 {
-	int *map;
-	t_elem *elem;
-	int b;
+	int		*map;
+	t_elem	*elem;
+	int		b;
 
-	if (argc != 2)
+	if (argc != 2 )
 		return (0);
 	if (!(map = malloc(sizeof(int) * 16)))
 		return (0);
 	elem = 0;
 	if (checkvalid_input(map, &elem, argv[1]))
-		printf ("§§§§§§§		ERROR		§§§§§§§\n");
+	{
+		printf ("§§§§§§§		MAP_ERROR		§§§§§§§\n");
+		ft_elemdel(&elem);
+	}
+		//////////////////////print/////////////////////////
 	else
 	{
 		t_elem *tmp = elem;
+		if (!tmp)
+			printf("#######		gg emptyfile	#######\n");
 		while (tmp)
 		{
 			b = 0;
